95feca934bad244285753144a97f5eedf2e96a1b
Gabriel Marinho <gdcmarinho@gmail.com>
2022-08-14 20:02:07 -0300

    adds semicolon and refactor port designation

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'content-type': 'text/html' })
     fs.createReadStream('index.html').pipe(res)
-})
+});
 
-server.listen(process.env.PORT || 3000)
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
